6c297f0e681702e578f50788598864c76d966872
Deniel Knezevic <deniel.knezevic@edu.fit.ba>
2022-07-05 21:19:11 +0200

    Added db connection string

--- RS2 Vjezba.Services/Database/eProdajaContext.cs
+++ RS2 Vjezba.Services/Database/eProdajaContext.cs
@@ -33,14 +33,6 @@ namespace RS2_Vjezba.Services.Database
         public virtual DbSet<Uloge> Uloges { get; set; } = null!;
         public virtual DbSet<VrsteProizvodum> VrsteProizvoda { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data source=localhost, 1434;Initial Catalog=eProdaja; user=sa; Password=HelloWorld123");
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
